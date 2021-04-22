0af6beb0ffd345fd0fa732e9a8222ddd0fdc56e1
Leonardo Moreno <lmoreno@tese.edu.mx>
2021-04-22 13:44:06 -0500

    ejemplos

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_widgets15751/pages/home_temp.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Widgets App',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp(),
+    );
+  }
+}
